2782f78d1c33cf025b609c9245c0079d6cb9603a
Md-Abdur-Razzak <razzak445444@gmail.com>
2023-11-05 23:45:22 +0600

    When the client clicks on Return books on the website, the book is deleted from that user's collection.

--- index.js
+++ index.js
@@ -29,13 +29,18 @@ async function run() {
     await client.connect();
    const catagoryCollection = client.db('libraryManagement').collection("catagory")
    const allBooksCollection = client.db('libraryManagement').collection("allBooks")
+   const borrowCollection = client.db('libraryManagement').collection("borrowdBooks")
   //  ----------get Data ----------------------------
    app.get("/catagory",async(req,res)=>{
     const result = await catagoryCollection.find().toArray()
     res.send(result)
    })
    app.get("/allbooks",async(req,res)=>{
-    const result = await allBooksCollection.find().toArray()
+  const result = await allBooksCollection.find().toArray()
+  res.send(result)
+   })
+   app.get("/borroBooks",async(req,res)=>{
+    const result = await borrowCollection.find().toArray()
     res.send(result)
    })
    app.get("/allbooks/:id",async(req,res)=>{
@@ -49,6 +54,20 @@ async function run() {
     const body = req.body
     const result = await allBooksCollection.insertOne(body)
     res.send(result)
+   })
+  app.post("/borrowBook",async(req,res)=>{
+    const body = req.body
+    const result = await borrowCollection.insertOne(body)
+    res.send(result)
+    const productId = (req.body.id);
+    const product = await allBooksCollection.findOne({ _id:new ObjectId(productId )}); 
+    if (product.quantity > 0) {
+      // Decrement the quantity by 1
+      product.quantity -= 1;
+      // Update the product in the database
+      await allBooksCollection.updateOne({ _id:new ObjectId(productId ) }, { $set: { quantity: product.quantity } });
+     
+    }
    })
   //   const {img,name,Aname,catagory,rating,_id}=updateBook
   app.post("/bookUpdate/:id",async(req,res)=>{
@@ -68,6 +87,25 @@ async function run() {
     const result = await allBooksCollection.updateOne(qurey,filter,option)
     res.send(result)
    })
+  //  ----------------delete----------------------------------------
+  app.post("/return/:id",async(req,res)=>{
+    const id = req.params.id
+    const deletId = req.body.deletId
+    console.log(deletId);
+    const quantityBook = await allBooksCollection.findOne({_id:new ObjectId(deletId)})
+   if(quantityBook){
+    quantityBook.quantity += 1
+    const d = await allBooksCollection.updateOne({_id:new ObjectId(deletId)},{ $set: { quantity:quantityBook.quantity } })
+   console.log(d);
+  }
+   
+    const qurey = {_id:new ObjectId(id)}   
+  
+ 
+    const result = await borrowCollection.deleteOne(qurey)
+    res.send(result)
+ 
+  })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
