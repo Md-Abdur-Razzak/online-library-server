7e77beea43595bf6042b9b427e07c3a29bf1bf1a
Md-Abdur-Razzak <razzak445444@gmail.com>
2023-11-05 08:37:55 +0600

    Categories are fetched from the database through the data get and sent to the client site

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+   const catagoryCollection = client.db('libraryManagement').collection("catagory")
+   app.get("/catagory",async(req,res)=>{
+    const result = await catagoryCollection.find().toArray()
+    res.send(result)
+   })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
